cf4bbeaf4bd9b75b0474233de11b86856b202893
Siwoo <skim327@myseneca.ca>
2018-04-12 00:21:51 -0400

    first deploy

--- src/main/backend/com/siwoo/webappag1/RootConfig.java
+++ src/main/backend/com/siwoo/webappag1/RootConfig.java
@@ -2,9 +2,11 @@ package com.siwoo.webappag1;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Profile("prod")
 @Configuration
 @EnableTransactionManagement
 @EntityScan("com.siwoo.webappag1.domain")
--- src/main/backend/com/siwoo/webappag1/WebConfig.java
+++ src/main/backend/com/siwoo/webappag1/WebConfig.java
@@ -3,6 +3,7 @@ package com.siwoo.webappag1;
 import com.siwoo.webappag1.converter.JsonLocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
+@Profile("prod")
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
 
