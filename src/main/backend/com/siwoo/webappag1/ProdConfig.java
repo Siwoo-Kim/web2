d42f440a6c8b1a6572484e60c6cb24131f82516a
Siwoo <skim327@myseneca.ca>
2018-04-12 00:31:27 -0400

    first deploy

--- src/main/backend/com/siwoo/webappag1/ProdConfig.java
+++ src/main/backend/com/siwoo/webappag1/ProdConfig.java
@@ -1,5 +1,6 @@
 package com.siwoo.webappag1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +9,15 @@ import org.springframework.context.annotation.Profile;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+@Slf4j
 @Profile("prod")
 @Configuration
 public class ProdConfig {
 
     @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
+    public BasicDataSource dataSource() {
+        try{
+
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
@@ -23,5 +27,11 @@ public class ProdConfig {
         basicDataSource.setUsername(username);
         basicDataSource.setPassword(password);
         return basicDataSource;
+
+        }catch (URISyntaxException e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
